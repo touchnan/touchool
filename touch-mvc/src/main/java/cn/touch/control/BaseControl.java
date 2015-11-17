4186196f71d148fdad3dbb31c3c65ee13e118e5c
touchnan <88052350@qq.com>
2015-11-17 16:00:11 +0800

    xss defence

--- touch-mvc/src/main/java/cn/touch/control/BaseControl.java
+++ touch-mvc/src/main/java/cn/touch/control/BaseControl.java
@@ -3,15 +3,19 @@
  */
 package cn.touch.control;
 
+import java.beans.PropertyEditorSupport;
 import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 /**
  * Mar 10, 2015
@@ -59,4 +63,25 @@ public class BaseControl {
         }
 
     }
+    
+	/**
+	 * 初始化数据绑定
+	 * 1. 将所有传递进来的String进行HTML编码，防止XSS攻击
+	 * 2. 将字段中Date类型转换为String类型
+	 */
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		// String类型转换，将所有传递进来的String进行HTML编码，防止XSS攻击
+		binder.registerCustomEditor(String.class, new PropertyEditorSupport() {
+			@Override
+			public void setAsText(String text) {
+				setValue(text == null ? null : StringEscapeUtils.escapeHtml4(text.trim()));
+			}
+			@Override
+			public String getAsText() {
+				Object value = getValue();
+				return value != null ? value.toString() : "";
+			}
+		});
+	}      
 }
--- touch-mvc/src/main/java/cn/touch/control/WelcomeControl.java
+++ touch-mvc/src/main/java/cn/touch/control/WelcomeControl.java
@@ -3,11 +3,16 @@
  */
 package cn.touch.control;
 
+import java.beans.PropertyEditorSupport;
+
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -63,4 +68,25 @@ public class WelcomeControl {
         
         return "welcome";
     }
+    
+	/**
+	 * 初始化数据绑定
+	 * 1. 将所有传递进来的String进行HTML编码，防止XSS攻击
+	 * 2. 将字段中Date类型转换为String类型
+	 */
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		// String类型转换，将所有传递进来的String进行HTML编码，防止XSS攻击
+		binder.registerCustomEditor(String.class, new PropertyEditorSupport() {
+			@Override
+			public void setAsText(String text) {
+				setValue(text == null ? null : StringEscapeUtils.escapeHtml4(text.trim()));
+			}
+			@Override
+			public String getAsText() {
+				Object value = getValue();
+				return value != null ? value.toString() : "";
+			}
+		});
+	}    
 }
