a9da0f5362da74c7791e8e7223cd19b6ab08dd6b
touchnan <88052350@qq.com>
2018-08-23 19:37:18 +0800

    datasource ,unresolvable circular reference,exclude DataSourceAutoConfiguration.class

--- touch-bootweb/src/main/java/cn/touch/bootweb/TouchBootwebApplication.java
+++ touch-bootweb/src/main/java/cn/touch/bootweb/TouchBootwebApplication.java
@@ -2,12 +2,11 @@ package cn.touch.bootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 import java.util.concurrent.CountDownLatch;
-
-@SpringBootApplication(scanBasePackages = {"cn.touch.bootweb.config"})
 
 /**
  * https://www.cnblogs.com/s648667069/p/6513335.html
@@ -16,12 +15,17 @@ import java.util.concurrent.CountDownLatch;
  * https://www.jianshu.com/p/3c94d7e76998?utm_source=oschina-app
  * https://blog.csdn.net/J3oker/article/details/52556008
  */
+@SpringBootApplication(scanBasePackages = {"cn.touch.bootweb.config"},exclude = {DataSourceAutoConfiguration.class})
 public class TouchBootwebApplication {
 
 
 //	public static final CountDownLatch downLatch = new CountDownLatch(1);
 	public static void main(String[] args) {
 		SpringApplication.run(TouchBootwebApplication.class, args);
+
+//		org.springframework.boot.autoconfigure.aop.AopAutoConfiguration
+//		org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration
+//		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 
 //		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 //			System.out.println("TouchBootwebApplication.main");
